b4ccf982191c5fa8d48b2346dfb0e9e74085b44b
root <jonassilvat.i@hotmail.com>
2020-05-14 21:30:58 -0300

    Criando uma calculadora usando ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Calculator from "./main/Calculator";
+
+function App() {
+  return (
+    <>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
